ef72791ec35b83aadd5ca92130fce971a4ea4c30
Rafey Rana <24100103@lums.edu.pk>
2024-07-23 12:16:21 +0500

    model outline done and testing done using the previous weights to verify the architecture

--- train.py
+++ train.py
@@ -14,6 +14,28 @@ class SelfAttention(nn.Module):
           self.c_projection = nn.Linear(config.n_embedding, config.n_embedding)
           self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size)).view(1,1, config.block_size, config.block_size()))
 
+        # register buffer is a way to store a tensor in the model. It is not a parameter of the model. It is a buffer. It is not trained. It is used for storing things like biases, running averages, etc.
+
+     def forward(self, x):
+          batch_size , seq_length, embedding_dim = x.size()
+          qkv = self.c_attention(x)
+          # so now we have 1024 tokens are lined up and they emit three vectors, the keys and values and the queries.
+          q, k ,v = qkv.split(self.n_embedding, dim=2)
+          # parallelisation for the multi headed attention
+          k = k.view(batch_size, seq_length, self.n_heads, embedding_dim // self.n_heads).transpose(1, 2)
+          q = q.view(batch_size, seq_length, self.n_heads, embedding_dim // self.n_heads).transpose(1, 2)
+          v = v.view(batch_size, seq_length, self.n_heads, embedding_dim // self.n_heads).transpose(1, 2)
+          attention = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
+          # masking the future tokens in training on the triangular matrix to -inf out the future tokens and only pay attention to previous context
+          attention = attention.masked_fill(self.bias[:,:,:seq_length, :seq_length] == 0, float('-inf'))
+          attention = F.softmax(attention, dim=-1) # normalising to 1
+          y = attention @ v
+          y = y.transpose(1, 2).contiguous().view(batch_size, seq_length, embedding_dim) # reassembling the heads output / concatenation tbh
+          # projecting output
+          y = self.c_projection(y)
+          return y 
+
+
 
 
 
@@ -35,7 +57,7 @@ class Block(nn.Module):
     def __init__(self, config):
         super().__init__()
         self.layer_normalisation_1 = nn.LayerNorm(config.n_embedding)
-        self.attention = CasualSelfAttention(config)
+        self.attention = SelfAttention(config)
         self.layer_normalisation_2 = nn.LayerNorm(config.n_embedding)
         self.feed_forward = MLP(config)
 
